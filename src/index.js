5b2f24bf07db748def66846d7f58b1de286da587
Osama Ameer <osama.ameer@metafroliclabs.com>
2024-08-11 09:03:37 +0500

    update github paages, react-router config

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { ArticleProvider } from "./context/ArticleContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/news-app">
       <ArticleProvider>
         <App />
       </ArticleProvider>
